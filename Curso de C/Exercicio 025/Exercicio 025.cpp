aac6830e0a66a4b2a2d0ea98a592272ec1c31c5d
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2020-04-11 18:35:27 -0300

    Exercicio 025

---
+++ Curso de C/Exercicio 025/Exercicio 025.cpp
@@ -0,0 +1,16 @@
+// Exercicio 025.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+int main()
+{
+	for (int i = 10; i >= 0; i--)
+	{
+		printf("%d \n", i);
+	}
+	system("pause");
+	return 0;
+}
+
+
