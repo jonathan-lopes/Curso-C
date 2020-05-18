5c3d7df52521ae973ae20cacf6433fbfd9bef1b1
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2020-05-18 00:48:48 -0300

    do while modificado

--- Curso de C/Desafio_Olimpico/Desafio_Olimpico/Desafio_Olimpico.c
+++ Curso de C/Desafio_Olimpico/Desafio_Olimpico/Desafio_Olimpico.c
@@ -6,50 +6,53 @@
 
 int main()
 {
-	int consumo, cont_agua, opcao;
-	
+	int consumo, cont_agua, c;
+	char opcao;
+
 	setlocale(LC_ALL, "portuguese");
 
 	do
 	{
 		system("cls");
 
-		printf("Digite o consumo de água da sua residência: ");
+		printf("Digite o consumo de Ã¡gua da sua residÃªncia: ");
 		scanf_s("%d", &consumo);
+		while ((c = getchar()) != '\n' && c != EOF) {}
 
 		if ((consumo >= 0) && (consumo <= 10))
 		{
-			printf("O valor da sua conta é: %d,00 R$ \n\n", ASSI_BASIC);
+			printf("O valor da sua conta Ã©: %d,00 R$ \n\n", ASSI_BASIC);
 		}
 		else
 		{
 			if ((consumo >= 11) && (consumo <= 30))
 			{
 				cont_agua = (consumo - 10) + ASSI_BASIC;
-				printf("O valor da sua conta é: %d,00 R$ \n\n", cont_agua);
+				printf("O valor da sua conta Ã©: %d,00 R$ \n\n", cont_agua);
 			}
 			else
 			{
 				if ((consumo >= 31) && (consumo <= 100))
 				{
 					cont_agua = ((consumo - 30) * 2) + 20 + ASSI_BASIC;
-					printf("O valor da sua conta é: %d,00 R$ \n\n", cont_agua);
+					printf("O valor da sua conta Ã©: %d,00 R$ \n\n", cont_agua);
 				}
 				else
 				{
 					if (consumo > 101)
 					{
 						cont_agua = (consumo - 100) * 5 + 60 + ASSI_BASIC;
-						printf("O valor da sua conta é: %d,00 R$ \n\n", cont_agua);
+						printf("O valor da sua conta Ã©: %d,00 R$ \n\n", cont_agua);
 					}
 				}
 			}
 		}
 
-		printf("Deseja calcular o valor de outra conta de água? ( 1 - Sim | 2 - Não ) \n");
-		scanf_s("%d", &opcao);
+		printf("Deseja calcular o valor de outra conta de Ã©gua? ( s - Sim | n - NÃ£o ) \n");
+		opcao = getchar();
+		while ((c = getchar()) != '\n' && c != EOF) {}
 
-	} while (opcao == 1);
+	} while (opcao == 's');
 
 	system("pause");
 	return 0;
